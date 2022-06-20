286644288a6825708f7fa0a9f04cd265a2104532
GustavoBurnsSoares <gburnssoares@gmail.com>
2022-06-19 23:56:00 -0300

    Adicionada a classe Position

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Position posicao = new Position(3, 5);
+		System.out.println(posicao);
+	}
+
+}
