cc735df9baabb01704baed33402409bd0b6fe849
GustavoBurnsSoares <gburnssoares@gmail.com>
2022-06-26 22:51:42 -0300

    Melhoria na exibição do tabuleiro

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -6,13 +6,41 @@ public class ChessPosition {
 	private char column;
 	private int row;
 
+	public ChessPosition(char column, int row) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Error instantiating ChessPosition. Valid values are a1 to h8.");
+		}
+		this.column = column;
+		this.row = row;
+	}
+
+	public char getColumn() {
+		return column;
+	}
+
+	public void setColumn(char column) {
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
 	protected Position toPosition() {
-		Position position = new Position(row, column);
-		return position;
+		return new Position(8 - row, column - 'a');
 	}
 
 	protected static ChessPosition fromPosition(Position position) {
-		ChessPosition chessPosition = new ChessPosition();
-		return chessPosition;
+		return new ChessPosition((char) ('a' - position.getRow()), 8 - position.getColumn());
 	}
+
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
+
 }
